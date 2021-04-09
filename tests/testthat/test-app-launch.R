2c4d903a7dc43be255ea5aa56c7b1150707afc09
Rhian Davies <rhian@jumpingrivers.com>
2021-04-09 09:13:02 +0100

    Test title only on Linux

--- tests/testthat/test-app-launch.R
+++ tests/testthat/test-app-launch.R
@@ -1,4 +1,5 @@
 test_that("The app runs", {
+  skip_on_os(c("windows", "mac"))
   app = shinytest::ShinyDriver$new(system.file("app", package = "appinabox"))
   expect_equal(app$getTitle(), "Praise")
   app$stop()
